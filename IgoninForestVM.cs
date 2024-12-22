f6c6961c7e01681fb95e4e0b0bbea73186fc4d08
1st-Juchara <136792261+1st-Juchara@users.noreply.github.com>
2024-12-22 11:52:07 +0300

    Lab_6_start

---
+++ IgoninForestVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igonin_Lab_6
+{
+  internal class IgoninForestVM : ViewModelBase
+  {
+    private ObservableCollection<string> strings = new ObservableCollection<string>();
+    public ObservableCollection<string> Strings {  get => strings; set => strings = value; }
+
+    private bool checkBind = false;
+    public bool CheckBind { get => checkBind; set => Set(ref checkBind, value); }
+
+    private int selectedInx = 0;
+    public int SelectedInx { get => selectedInx; set => Set(ref selectedInx, value); }
+
+    public IgoninForestVM() 
+    {
+      Strings.Add("First Line");
+
+      Strings.Add("Second Line");
+
+      Strings.Add("Third Line");
+
+    }
+    public void AddString()
+    {
+      Strings.Add($"Line number: {Strings.Count + 1}");
+    }
+
+    public void CheckBindCheck()
+    { 
+      CheckBind = !CheckBind;
+    }
+  }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Igonin_Lab_6
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    IgoninForestVM forest = new IgoninForestVM();
+    
+    [DllImport("IgoninForestDll.dll")]
+    public static extern IntPtr Create();
+
+    [DllImport("IgoninForestDll.dll")]
+    public static extern void SetAge(IntPtr animal, int newAge);
+
+    [DllImport("IgoninForestDll.dll")]
+    public static extern int GetParams(IntPtr animal);
+
+    [DllImport("IgoninForestDll.dll", CharSet = CharSet.Unicode)]
+    public static extern IntPtr GetName(IntPtr animal);
+
+    [DllImport("IgoninForestDll.dll", CharSet = CharSet.Unicode)]
+    public static extern void SetName(IntPtr animal, string newName);
+
+    [DllImport("IgoninForestDll.dll")]
+    public static extern void FreeMemory(IntPtr ptr);
+
+    public MainWindow()
+    {
+      DataContext = forest;
+      InitializeComponent();
+      IntPtr an = Create();
+      SetAge(an, 10);
+      int an_age = GetParams(an);
+
+      string new_name = "Ime4ko";
+      SetName(an, new_name);
+      IntPtr ptr = GetName(an);
+      string an_name = Marshal.PtrToStringUni(ptr);
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      forest.AddString();
+    }
+  }
+}
